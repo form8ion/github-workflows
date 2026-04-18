bc799bbdc068759da84fd3bfef12bd13f0cbd3e3
Matt Travi <programmer@travi.org>
2026-04-18 17:14:29 -0500

    test(lint): upgrade to the beta of the eslint config

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,6 +1,6 @@
 import {promises as fs} from 'node:fs';
 
-export default async function ({projectRoot}) {
+export default async function scaffoldGithubWorkflows({projectRoot}) {
   await fs.mkdir(`${projectRoot}/.github/workflows`, {recursive: true});
 
   return {};
